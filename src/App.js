e33cabad0bbf29d8dae106f0b375cffb4f504f80
Uukow <93025470+Uukow@users.noreply.github.com>
2024-08-18 10:05:30 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './assets/main.css';
+// import './assets/main.css';
 import './assets/style.css';
 import './Styles/Navigation.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
